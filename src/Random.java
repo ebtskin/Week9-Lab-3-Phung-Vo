f9ef1bbc8328598b8a0dd931ece5c5646fcaa3dd
Phung Vo <ebtskin@gmail.com>
2015-10-27 23:04:50 -0700

    Week9 Lab 3

---
+++ src/Random.java
@@ -0,0 +1,12 @@
+// Random Object Created
+
+
+public class Random
+{
+
+    public int GetANumber_Between_1_and_10()
+    {
+        int gameNum = 1 + (int)(Math.random()*10);
+        return gameNum;
+    }
+}
